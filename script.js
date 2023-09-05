abff102d5cbdf76a8c22803d53f3563f495a3c4b
sevaranurnazarova <sevaranurnazarova2005@gmail.com>
2023-09-05 19:59:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let a = 5;
+// let b = 2;
+// let c = 1;
+
+// let natija;
+
+// natija = a + b + c;
+
+// alert(natija);
+// console.log(natija);
+// document.write(natija);
+
+let a = 12085;
+let sum = prompt("$ kiriting")
+console.log(Number(a * sum));
